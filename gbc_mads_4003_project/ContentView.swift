de03d3fde4501aeb3139a8b3aff8c546d839f257
Wesley Hui <wesleyhuichunwai@gmail.com>
2025-10-03 19:13:08 -0400

    Initial Commit

---
+++ gbc_mads_4003_project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  gbc_mads_4003_project
+//
+//  Created by user on 2025-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gbc_mads_4003_project/gbc_mads_4003_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  gbc_mads_4003_projectApp.swift
+//  gbc_mads_4003_project
+//
+//  Created by user on 2025-10-03.
+//
+
+import SwiftUI
+
+@main
+struct gbc_mads_4003_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
