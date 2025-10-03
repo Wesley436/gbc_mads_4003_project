a478c1960f9668be10b951e432c46ed6e089f357
Wesley Hui <wesleyhuichunwai@gmail.com>
2025-10-03 19:35:38 -0400

    User and Session classes

---
+++ gbc_mads_4003_project/Session.swift
@@ -0,0 +1,30 @@
+//
+//  Session.swift
+//  gbc_mads_4003_project
+//
+//  Created by user on 2025-10-03.
+//
+
+import Foundation
+
+class Session {
+    var id: Int
+    var name: String
+    var description: String
+    var starRating: Int
+    var hostName: String
+    var photos: [String]
+    var pricePerPerson: Int
+    var phoneNumber: String
+    
+    init (id: Int, name: String, description: String, starRating: Int, hostName: String, photos: [String], pricePerPerson: Int, phoneNumber: String) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.starRating = starRating
+        self.hostName = hostName
+        self.photos = photos
+        self.pricePerPerson = pricePerPerson
+        self.phoneNumber = phoneNumber
+    }
+}
---
+++ gbc_mads_4003_project/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  gbc_mads_4003_project
+//
+//  Created by user on 2025-10-03.
+//
+
+import Foundation
+
+class User {
+    var id: Int
+    var email: String
+    var password: String
+    
+    init (id: Int, email: String, password: String) {
+        self.id = id
+        self.email = email
+        self.password = password
+    }
+}
