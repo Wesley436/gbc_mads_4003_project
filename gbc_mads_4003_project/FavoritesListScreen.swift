01f150a585189e59543f710192e24cf51c9084dc
Wesley Hui <wesleyhuichunwai@gmail.com>
2025-10-10 11:53:30 -0400

    remove one and remove all favourite sessions

--- gbc_mads_4003_project/FavoritesListScreen.swift
+++ gbc_mads_4003_project/FavoritesListScreen.swift
@@ -11,6 +11,8 @@ import SwiftUI
 
 struct FavoritesListScreen: View {
     @State private var favoriteSessions : [Session] = []
+    @State private var userId: Int = -1
+    @State private var userFavourites: [Int:[Int]] = [:]
     
     let defaults = UserDefaults.standard
     
@@ -18,37 +20,55 @@ struct FavoritesListScreen: View {
         NavigationView {
             List {
                 ForEach(favoriteSessions) { session in
-                    NavigationLink(destination: SessionDetailsScreen(session: session)) {
-                        HStack(spacing: 15) {
-                            AsyncImage(url: URL(string: session.photos.first!)){ result in
-                                result.image?
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 80, height: 80)
-                                    .cornerRadius(10)
-                                    .clipped()
+                        NavigationLink(destination: SessionDetailsScreen(session: session)) {
+                            HStack(spacing: 15) {
+                                AsyncImage(url: URL(string: session.photos.first!)){ result in
+                                    result.image?
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 80, height: 80)
+                                        .cornerRadius(10)
+                                        .clipped()
+                                }
+                                
+                                VStack(alignment: .leading) {
+                                    Text(session.name)
+                                        .font(.headline)
+                                    
+                                    Text("$\(session.pricePerPerson) per person")
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                }
+                            }
+                            .padding(.vertical, 5)
+                        }
+                        
+                        Button(action: {
+                            if let index = userFavourites[userId]!.firstIndex(of: session.id) {
+                                userFavourites[userId]!.remove(at: index)
                             }
                             
-                            VStack(alignment: .leading) {
-                                Text(session.name)
-                                    .font(.headline)
-                                
-                                Text("$\(session.pricePerPerson) per person")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
+                            if let index = favoriteSessions.firstIndex(where: { $0.id == session.id }) {
+                                favoriteSessions.remove(at: index)
                             }
-                        }
-                        .padding(.vertical, 5)
-                    }
+                            if let encodedUserFavourites = try? JSONEncoder().encode(userFavourites) {
+                                defaults.set(encodedUserFavourites, forKey: "userFavourites")
+                            }
+                            
+                        }, label: {
+                            Text("Remove favourite")
+                            }
+                        )
                 }
             }
             .navigationTitle("Favourite Sessions")
         }
         .onAppear() {
             if let savedUser = defaults.data(forKey: "currentUser"), let decodedUser = try? JSONDecoder().decode(User.self, from: savedUser) {
-                let userId = decodedUser.id
+                userId = decodedUser.id
                 if let savedUserFavourites = defaults.data(forKey: "userFavourites"), let decodedUserFavourites = try? JSONDecoder().decode([Int:[Int]].self, from: savedUserFavourites) {
-                    let favouriteIds = decodedUserFavourites[userId] ?? []
+                    userFavourites = decodedUserFavourites
+                    let favouriteIds = userFavourites[userId] ?? []
                     favoriteSessions = SessionsManager.findSessionsByIds(ids: favouriteIds)
                 }
             }
@@ -56,8 +76,12 @@ struct FavoritesListScreen: View {
         
         //delete all button
         Button(action: {
-            self.deleteAllSessions()
-    
+            userFavourites[userId] = []
+            if let encodedUserFavourites = try? JSONEncoder().encode(userFavourites) {
+                defaults.set(encodedUserFavourites, forKey: "userFavourites")
+            }
+            
+            favoriteSessions = []
         }, label: {
             Text("Delete all Sessions")
                 .font(.subheadline)
@@ -66,10 +90,6 @@ struct FavoritesListScreen: View {
         .padding()
         .buttonStyle(.borderedProminent)
     }//end of body
-    
-    func deleteAllSessions(){
-        
-    }
 }
 
 
